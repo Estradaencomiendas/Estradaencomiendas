fd44dabaabcd1497916571d9b726d6aa60e104e0
Estradaencomiendas <marco.rp157@gmail.com>
2024-09-14 01:16:41 -0400

    Update sendSms.js

--- functions/sendSms.js
+++ functions/sendSms.js
@@ -1,19 +1,18 @@
 const twilio = require('twilio');
 
 // Credenciales de Twilio
-const accountSid = 'AC6c6fb4d7e40a2b8220bfb2349605166e';  // Reemplaza con tu Account SID de Twilio
-const authToken = '5d1221ef550c190993dd255c3dca8d8a';    // Reemplaza con tu Auth Token de Twilio
-
+const accountSid = 'AC6c6fb4d7e40a2b8220bfb2349605166e';  // Reemplaza con tu Account SID
+const authToken = 'd9bb49a6b1081d478988940da1a5a435';    // Reemplaza con tu Auth Token
 const client = twilio(accountSid, authToken);
 
 exports.handler = async function(event, context) {
-    const { to, trackingNumber } = JSON.parse(event.body);
+    const { to, trackingNumber } = JSON.parse(event.body);  // 'to' es el número de teléfono destinatario
 
     try {
         const message = await client.messages.create({
-            body: `Tu número de seguimiento es: ${trackingNumber}`,  // Mensaje personalizado
-            from: '+13863336602',  // Número de teléfono de Twilio
-            to: to  // Número de teléfono del destinatario
+            body: `Tu número de seguimiento es: ${trackingNumber}`,  // El cuerpo del mensaje
+            from: '+13863336602',  // El número de Twilio que compraste
+            to: to  // Número de teléfono del destinatario (debe estar en formato E.164, por ejemplo, +503xxxxxxxx)
         });
 
         return {
@@ -21,9 +20,4 @@ exports.handler = async function(event, context) {
             body: JSON.stringify({ message: 'Mensaje enviado con éxito', sid: message.sid }),
         };
     } catch (error) {
-        return {
-            statusCode: 500,
-            body: JSON.stringify({ error: 'Error enviando el mensaje' }),
-        };
-    }
-};
+        console.error('
