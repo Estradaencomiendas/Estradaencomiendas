d94b5bed181cc02de6020870177fd8a1e174f1fc
Estradaencomiendas <marco.rp157@gmail.com>
2024-09-14 00:01:54 -0400

    Add files via upload

---
+++ functions/sendSms.js
@@ -0,0 +1,29 @@
+const twilio = require('twilio');
+
+// Credenciales de Twilio
+const accountSid = 'AC6c6fb4d7e40a2b8220bfb2349605166e';  // Reemplaza con tu Account SID de Twilio
+const authToken = '5d1221ef550c190993dd255c3dca8d8a';    // Reemplaza con tu Auth Token de Twilio
+
+const client = twilio(accountSid, authToken);
+
+exports.handler = async function(event, context) {
+    const { to, trackingNumber } = JSON.parse(event.body);
+
+    try {
+        const message = await client.messages.create({
+            body: `Tu número de seguimiento es: ${trackingNumber}`,  // Mensaje personalizado
+            from: 'TU_NUMERO_TWILIO',  // Número de teléfono de Twilio
+            to: to  // Número de teléfono del destinatario
+        });
+
+        return {
+            statusCode: 200,
+            body: JSON.stringify({ message: 'Mensaje enviado con éxito', sid: message.sid }),
+        };
+    } catch (error) {
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ error: 'Error enviando el mensaje' }),
+        };
+    }
+};
