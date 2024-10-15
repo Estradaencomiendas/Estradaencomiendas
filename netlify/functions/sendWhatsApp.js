99044de23353acb5874e81db2b85243816066bbe
Estradaencomiendas <marco.rp157@gmail.com>
2024-10-15 17:56:21 -0400

    Update sendWhatsApp.js

--- netlify/functions/sendWhatsApp.js
+++ netlify/functions/sendWhatsApp.js
@@ -7,19 +7,21 @@ const client = twilio(accountSid, authToken);
 
 exports.handler = async function(event, context) {
     const data = JSON.parse(event.body);
-    const { to, trackingNumber, nombreCliente, destino, valor } = data;
+    const { to, trackingNumber, nombreCliente, destino, valor, nombrePaquete, qrUrl } = data; // Agregar 'nombrePaquete' y 'qrUrl'
 
     try {
-        // Enviar mensaje usando la plantilla aprobada
+        // Enviar mensaje usando la nueva plantilla aprobada
         const message = await client.messages.create({
             from: 'whatsapp:+50369228310', // Número de WhatsApp de Twilio
             to: `whatsapp:${to}`, // Número de la vendedora en formato de WhatsApp
-            contentSid: 'HXaa47c3432408116e5941f4fe37a809c5', // SID de la plantilla
+            contentSid: 'HXdd870bd497f5c1887cf678e04f589b8', // SID de la nueva plantilla
             contentVariables: JSON.stringify({
                 '1': trackingNumber,     // Variable 1 es el número de seguimiento
                 '2': nombreCliente,      // Variable 2 es el nombre de la clienta
                 '3': destino,            // Variable 3 es el destino
-                '4': valor               // Variable 4 es el valor del paquete
+                '4': valor,              // Variable 4 es el valor del paquete
+                '5': nombrePaquete,      // Nueva variable: Nombre de la Página
+                '6': qrUrl               // Nueva variable: URL del código QR
             })
         });
 
