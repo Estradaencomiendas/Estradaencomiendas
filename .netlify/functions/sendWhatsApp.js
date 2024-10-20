5943a49d8c6971748980bab7f31c35101a70a685
Estradaencomiendas <marco.rp157@gmail.com>
2024-10-20 12:22:42 -0400

    Update sendWhatsApp.js

--- .netlify/functions/sendWhatsApp.js
+++ .netlify/functions/sendWhatsApp.js
@@ -17,7 +17,7 @@ exports.handler = async function(event, context) {
         const message = await client.messages.create({
             from: 'whatsapp:+50369228310', // Número de WhatsApp de Twilio
             to: `whatsapp:${to}`, // Número de la vendedora en formato de WhatsApp
-            contentSid: 'Hxa6f731eef12b1eb1f8d1e5bcefb0b66b', // Content SID de tu plantilla aprobada
+            contentSid: 'HX5abb859370c97a6a19e758a53cc9626e', // Content SID de tu plantilla aprobada
             contentVariables: JSON.stringify({
                 '1': trackingNumber,     // Variable 1: número de seguimiento
                 '2': nombreCliente,      // Variable 2: nombre de la clienta
